2d9f97d1f57a4a95e769a9548856ced6e28c5889
Angie Munoz <angiepmunoz@gmail.com>
2021-07-15 14:30:01 -0400

    completed lab

--- index.js
+++ index.js
@@ -4,7 +4,39 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+function calculator() {
+    const operatorStr = process.argv[2];
+    let currentEval = Number(process.argv[3]);
+    if(operatorStr !== 'plus' && operatorStr !== 'minus'){ //will either be undefined or a diff operator or a number
+        console.log('MADE IT INSIDE')
+        if(!!Number(operatorStr)/1 || !operatorStr){ // if its divisible by 1 and does not result in NaN or is undefined
+            return "No operation provided...";
+        }else{
+            return `Invalid operation: ${operatorStr}`
+        };
+    }else if(!currentEval){
+        return "No numbers provided...";
+    };
+
+    if(operatorStr === 'plus'){ ///coundn't figure out how to do this more efficiently 
+        for(let i = 4; i< process.argv.length; i++){
+            let currentNum = Number(process.argv[i]);
+            currentEval += currentNum;
+        }
+    }else{
+        for(let i = 4; i< process.argv.length; i++){
+            let currentNum = Number(process.argv[i]);
+            currentEval -= currentNum;
+        }
+    }
+    return currentEval;
+}
+
+// process.argv[2] = 'modulu'
+// console.log(calculator());
+
+
+
 
 // Don't change anything below this line.
 module.exports = calculator;
---
+++ random.js
@@ -0,0 +1,4 @@
+let string = 'hello';
+let numStr = Number(string);
+console.log(typeof numStr);
+console.log(numStr/1)
